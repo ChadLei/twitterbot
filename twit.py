dafa2298ae0e59ed2034e53cd6c40164708a15fe
Chad Lei <cblei@uci.edu>
2019-07-25 14:21:48 -0700

    new changes

--- twit.py
+++ twit.py
@@ -6,49 +6,54 @@ consumer_secret = 'KjRrldyr03VN2iDKpMk905AYhZdBG8eHGV8dZDpPrQvTwZX28s'
 access_token = '1153774025711030273-zdqneNw0Epe8Sl8Wge7CZl2jqYpvTH'
 access_token_secret = 'x1Wdl35xJjzBUXpiSuWU7lPwGBz2k2LanCcmwlmp2rhqs'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+# User-authentication allows for 180 queries per access token every 15 minutes
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #Pass our consumer key and consumer secret to Tweepy's user authentication handler
+auth.set_access_token(access_token, access_token_secret) #Pass our access token and access secret to Tweepy's user authentication handler
+api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True) #Creating a twitter API wrapper using tweepy
 
-user = api.me()
+# Switch to application authentication - application-only authentication allows for 450 queries every 15 minutes
+# auth = tweepy.AppAuthHandler(consumer_key, consumer_secret) 
+# api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True) #Setting up new api wrapper, using authentication only
 
+# user = api.me()
 
 def main():
 	count = 0
+	# search = ("obama")
 	# search = ("like", "retweet", "win", "giveaway", "rt to win", "chance to win")
-	search = ("like and retweet OR like and rt OR retweet to win OR c OR rt to win OR chance to win OR giving away -filter:retweets")
-	numOfTweets = 100
-	for tweet in tweepy.Cursor(api.search, search, tweet_mode='extended').items(numOfTweets):
+	search = ("#rttowin OR #giveaway OR giveaway OR like and retweet OR like and rt OR retweet to enter OR retweet to win OR c OR rt to win OR chance to win OR giving away OR win free OR win a free OR chance to win -filter:retweets")
+	numOfTweets = 1000 #Maximum number of tweets we want to collect 
+	for tweet in tweepy.Cursor(api.search, search, count=100, tweet_mode='extended').items(numOfTweets):
 		try:
-			# tweet.retweet()
-			# print("Retweeted")
-			print(tweet.user.screen_name)
-			# print(tweet.user.id)
-			if 'retweeted_status' in tweet._json:
-				# print(tweet._json['retweeted_status']['full_text'])
-				retweet_text = 'RT @ ' + api.get_user(tweet.retweeted_status.user.id_str).screen_name
-				print(retweet_text)
-			else:
-				tweetText = tweet.full_text
-				wackWords = ['follow', 'Follow', 'FOLLOW', 'comment', 'Comment', 'COMMENT', 'tag', 'Tag', 'TAG', 'survey']
-				if  any(word not in tweetText for word in wackWords):
-					print(tweetText + '\n')
-					if 'like' in tweetText or 'Like' in tweetText or 'LIKE' in tweetText:
-						tweet.favorite()
-						print("[Liked]")
-					if 'retweet' in tweetText or 'Retweet' in tweetText or 'RETWEET' in tweetText or 'rt' in tweetText or 'RT' in tweetText:
-						tweet.retweet()
-						print("[Retweeted]")
-					count += 1
-					print("Number of tweets gona through: " + str(count) + '\n')
-					time.sleep(120)
-					# api.create_friendship(tweet.user.id)
+			print("[USERNAME: " + tweet.user.screen_name + "]")
+			tweetText = tweet.full_text
+			wackWords = ['follow', 'comment', 'tag', 'survey', 'fill out']
+			# print(tweetText)
+			if  all(word not in tweetText.lower() for word in wackWords):
+				print(tweetText.lower())
+				if 'like' in tweetText.lower():
+					tweet.favorite()
+					print("[Liked]")
+				if 'retweet' in tweetText.lower() or 'rt' in tweetText.lower():
+					tweet.retweet()
+					print("[Retweeted]")
+				if 'tag' in tweetText.lower():
+					api.update_status('@officialchidori the first of course!!', in_reply_to_status_id=tweet.id)
+					print("[Tagged]")
+				count += 1
+				print("[Number of tweets gone through: " + str(count) + ']\n')
+				time.sleep(180)
+				# api.create_friendship(tweet.user.id)
+			
 		except tweepy.TweepError as e:
-			# print(e.reason)
-			print("[I have already liked/retweeted, so moving on....]")
-			time.sleep(60)
+			print(e.reason + '\n')
+			# print("[This has already been liked/retweeted, so moving on....]\n")
+			# time.sleep(60)
 			continue
 		except StopIteration:
+			print("[I have stopped for some reason....]")
 			break
+	# print(api.rate_limit_status()['resources']['search']) #You can check how many queries you have left using rate_limit_status() method
+	print("[Complete!]")
 
 main()
